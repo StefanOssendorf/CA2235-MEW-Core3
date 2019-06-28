2c3b94f9f2c6cd273868678da652df65d4b6d720
Stefan Ossendorf <StefanOssendorf@users.noreply.github.com>
2019-06-28 11:06:51 +0200

    Initial commit with mwe

---
+++ CA2235Repro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CA2235Repro
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+
+    [Serializable]
+    public class A
+    {
+        public String B;
+
+        public String C { get; set; }
+    }
+}
